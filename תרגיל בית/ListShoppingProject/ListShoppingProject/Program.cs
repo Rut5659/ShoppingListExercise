b94666034507771ee7425e04d9c4a1392d91d994
Ruti Shaharabani <rutish5659@gmail.com>
2025-06-24 23:15:32 +0300

    start commit

---
+++ תרגיל בית/ListShoppingProject/ListShoppingProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListShoppingProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
